18ed8f3dcc954b4a6eaef8ae50e171cee78dd2f7
maxrabin <max.rabin17@gmail.com>
2021-05-06 14:33:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let field = document.querySelector("#new-task-description")
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    let getUl = document.getElementById("tasks");
+    let list = document.createElement('li');
+    getUl.append(list);
+    deleteFunction();
+
+  })
 });
+
+// 1. Grab task ul
+// 2. Create li with text from user input
+// 3. append the li to the ul
+
+function deleteFunction {
+  addEventListener("click", () =>{
+    document.getUl.reset()
+  })
+}
